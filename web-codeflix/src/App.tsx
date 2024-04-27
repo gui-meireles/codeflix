592e7a6469b2b0513babc5f5693be5c67685f360
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-04-27 16:49:32 -0300

    Ajustes App e criação das paginas de categoria

--- web-codeflix/src/App.tsx
+++ web-codeflix/src/App.tsx
@@ -4,23 +4,10 @@ import {Box, ThemeProvider, Typography} from "@mui/material";
 import {Header} from "./components/Header";
 import {Layout} from "./components/Layout";
 import {appTheme} from "./config/theme";
-import { Routes, Route, Link} from "react-router-dom";
-
-const Home = () => (
-    <Box>
-        <Typography variant="h3" component="h1">
-            Home
-        </Typography>
-    </Box>
-);
-
-const About = () => (
-    <Box>
-        <Typography variant="h3" component="h1">
-            About
-        </Typography>
-    </Box>
-);
+import {Routes, Route} from "react-router-dom";
+import {CategoryList} from "./features/categories/ListCategory";
+import {CategoryCreate} from "./features/categories/CreateCategory";
+import {CategoryEdit} from "./features/categories/EditCategory";
 
 function App() {
     return (
@@ -34,8 +21,17 @@ function App() {
                 <Layout>
                     <h1>Welcome to React</h1>
                     <Routes>
-                        <Route path="/" element={<Home/>} />
-                        <Route path="about" element={<About/>} />
+                        <Route path="/" element={<CategoryList/>}/>
+                        <Route path="/categories" element={<CategoryList/>}/>
+                        <Route path="/categories/create" element={<CategoryCreate/>}/>
+                        <Route path="/categories/edit/:id" element={<CategoryEdit/>}/>
+
+                        <Route path="*" element={
+                            <Box sx={{ color: "white" }}>
+                                <Typography variant="h1">404</Typography>
+                                <Typography variant="h2">Page not found</Typography>
+                            </Box>
+                        }/>
                     </Routes>
                 </Layout>
             </Box>
