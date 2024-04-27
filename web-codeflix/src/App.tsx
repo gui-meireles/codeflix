49aa5a6bfca23d0ff2fb8b40d8cc5a68078ac90a
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-04-27 10:26:09 -0300

    Aplicação codeflix inicial, rotas e Layout

---
+++ web-codeflix/src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import {Box, ThemeProvider, Typography} from "@mui/material";
+import {Header} from "./components/Header";
+import {Layout} from "./components/Layout";
+import {appTheme} from "./config/theme";
+import { Routes, Route, Link} from "react-router-dom";
+
+const Home = () => (
+    <Box>
+        <Typography variant="h3" component="h1">
+            Home
+        </Typography>
+    </Box>
+);
+
+const About = () => (
+    <Box>
+        <Typography variant="h3" component="h1">
+            About
+        </Typography>
+    </Box>
+);
+
+function App() {
+    return (
+        <ThemeProvider theme={appTheme}>
+            <Box component="main"
+                 sx={{
+                     height: "100vh",
+                     backgroundColor: (theme) => theme.palette.grey[900],
+                 }}>
+                <Header/>
+                <Layout>
+                    <h1>Welcome to React</h1>
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="about" element={<About/>} />
+                    </Routes>
+                </Layout>
+            </Box>
+        </ThemeProvider>
+    );
+}
+
+export default App;
